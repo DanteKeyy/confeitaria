ce4963ea0f6ac7b7df5f1ec35a9282fe347d775e
DanteKeyy <95089533+DanteKeyy@users.noreply.github.com>
2024-07-26 10:59:47 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/index")
+
+@app.route("/")
 def inicial():
     return render_template("index.html")
+
 
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
+
 
 if __name__ == '__main__':
     app.run()
